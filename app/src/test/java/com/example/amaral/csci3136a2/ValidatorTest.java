d0a75ede60916f21f874169bd18860b2c4b6991f
Ryan Amaral <ryan_amaral@live.com>
2018-02-06 21:16:40 -0400

    added test for not password

--- app/src/test/java/com/example/amaral/csci3136a2/ValidatorTest.java
+++ app/src/test/java/com/example/amaral/csci3136a2/ValidatorTest.java
@@ -13,11 +13,20 @@ public class ValidatorTest {
 
     Validator vldtr = new Validator();
 
-    @Test // test whether password is not "password"
+    @Test // test validator as a whole
     public void testValidator() throws Exception {
         assertEquals(1, vldtr.validate("passwor"));
         assertEquals(1, vldtr.validate("password"));
         assertEquals(2, vldtr.validate("12345678"));
         assertEquals(2, vldtr.validate("passwords"));
+    }
+
+    @Test // test not "password" subroutine
+    public void testNotPassword() throws Exception{
+        assertEquals(true, vldtr.valNotPassord("9sdhf9sd8hf98sdhf"));
+        assertEquals(false, vldtr.valNotPassord("password"));
+        assertEquals(false, vldtr.valNotPassord("Password"));
+        assertEquals(false, vldtr.valNotPassord("PASSWORD"));
+        assertEquals(true, vldtr.valNotPassord("passwords"));
     }
 }
